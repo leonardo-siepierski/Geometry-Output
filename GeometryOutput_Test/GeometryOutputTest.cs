694857af6e0f184e009b733d26aeb559b76cb74f
Leonardo Siepierski <leonardo.siepierski@intelitrader.com.br>
2023-01-14 10:47:57 -0300

    test: adiciona teste que verifica pdf

--- GeometryOutput_Test/GeometryOutputTest.cs
+++ GeometryOutput_Test/GeometryOutputTest.cs
@@ -1,6 +1,5 @@
 using FluentAssertions;
 using Geometry_Output;
-using Microsoft.VisualStudio.TestPlatform.Utilities;
 
 namespace GeometryOutput_Test
 {
@@ -32,6 +31,28 @@ namespace GeometryOutput_Test
             var output = consoleOutput.ToString();
 
             output.Should().Contain(expectedOutput);
+        }
+
+        [Fact]
+        public void GeneratePdf_ShouldCreateFile()
+        {
+            GeometryOutput.Diamond = "   A   \r\n" +
+            "  B B  \r\n" +
+            " C   C \r\n" +
+            "D     D\r\n" +
+            " C   C \r\n" +
+            "  B B  \r\n" +
+            "   A   ";
+
+            string filePath = "Diamond.pdf";
+
+            if (File.Exists(filePath))
+            {
+                File.Delete(filePath);
+            }
+
+            GeometryOutput.GeneratePdf();
+            File.Exists(filePath).Should().BeTrue();
         }
     }
 }
